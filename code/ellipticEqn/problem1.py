efc007549d3adb8a753cd3279d6eaa7a6c7ee394
ymma98 <ymma98@qq.com>
2023-07-03 00:15:42 +0800

    always convert matA to lil format after (matA + matA1 + ...)

--- code/ellipticEqn/problem1.py
+++ code/ellipticEqn/problem1.py
@@ -34,9 +34,12 @@ from mesh import TriMesh2d, BoundaryData2d
 import fem2d
 import scipy.sparse.linalg
 import time
+from numba import jit
 np.set_printoptions(threshold=sys.maxsize)
 
 def problem1(Nlb = 3):
+    
+    @jit(nopython=True, cache=True)
     def boundary_condition(x, y):
         tol = 1.e-14
         res = 0.
@@ -55,14 +58,21 @@ def problem1(Nlb = 3):
         else:
             raise ValueError("point is not on the boundary.")
 
+    @jit(nopython=True, cache=True)
     def exact_sol(x, y):
         res = x * y * (1. - x/2.) * (1. - y) * np.exp(x + y)
         return res
+
+    time1 = time.time()
+
     # generate mesh grid, FE data, and BC data
     x1d_mesh_node = np.linspace(-1., 1., 257)
     y1d_mesh_node = np.linspace(-1., 1., 257)
     mesh2d = TriMesh2d(x1d_mesh_node, y1d_mesh_node, FE_node_num=Nlb)
     bc_data = BoundaryData2d(mesh2d.P, mesh2d.CT, mesh2d.Pb, mesh2d.CTb)
+
+    time2 = time.time()
+    print("generate mesh and BC data -> time usage = %f sec" % (time2 - time1))
 
     # assemble the matrix
     # cfunc_num = 1 corresponds to coeff_func(x,y) = 1
@@ -74,6 +84,9 @@ def problem1(Nlb = 3):
                           trial_func_ndx = 1,  trial_func_ndy = 0,  \
                           test_func_ndx = 1,   test_func_ndy = 0)
 
+    time3 = time.time()
+    print("generate 1st matA -> time usage = %f sec" % (time3 - time2))
+
     # print(matA.toarray())
     matA1 = fem2d.get_matA(cfunc_num = 1, N = mesh2d.N, Nb = mesh2d.Nb,
                           matP = mesh2d.P, CmatT = mesh2d.CT, 
@@ -82,7 +95,14 @@ def problem1(Nlb = 3):
                           Nlb_trial = mesh2d.Nlb,  Nlb_test = mesh2d.Nlb, \
                           trial_func_ndx = 0,  trial_func_ndy = 1,  \
                           test_func_ndx = 0,   test_func_ndy = 1)
-    matA = matA + matA1
+
+    time4 = time.time()
+    print("generate 2nd matA -> time usage = %f sec" % (time4 - time3))
+
+    matA = (matA + matA1).tolil()
+
+    time5 = time.time()
+    print("add matA1 and matA2 -> time usage = %f sec" % (time5 - time4))
 
     # print(matA.toarray())
     # cfunc_num = 2 corresponds to coeff_func(x,y) = f(x,y) in problem 1
@@ -92,6 +112,8 @@ def problem1(Nlb = 3):
                            Nlb_test = mesh2d.Nlb, \
                            test_func_ndx = 0,  test_func_ndy = 0)
 
+    time6 = time.time()
+    print("generate vecb -> time usage = %f sec" % (time6 - time5))
     # print(np.asarray(vecb))
 
     # treate Dirichlet BC
@@ -103,11 +125,18 @@ def problem1(Nlb = 3):
             vecb[temp_node_num] = boundary_condition(mesh2d.Pb[0,temp_node_num],\
                                                      mesh2d.Pb[1,temp_node_num])
 
+    time7 = time.time()
+    print("treate Dirichlet BC -> time usage = %f sec" % (time7 - time6))
+
     # solve A * x = b
     matA = matA.tocsr()
     vecx = scipy.sparse.linalg.spsolve(matA, vecb)
 
-    print(matA.shape)
+
+    time8 = time.time()
+    print("solve Ax = b -> time usage = %f sec" % (time8 - time7))
+
+    print("shape of matA", matA.shape)
 
     # estimate error
     err = np.zeros(mesh2d.Pb.shape[1])
@@ -118,13 +147,16 @@ def problem1(Nlb = 3):
     print("%.5e" % (np.amax(err)))
     print("*****" * 5)
 
+    time9 = time.time()
+    print("estimate absolute error -> time usage = %f sec" % (time9 - time8))
+
 
     pass
 
 
 if __name__ == '__main__':
     start_time = time.time()
-    problem1(Nlb=3)
+    problem1(Nlb=6)
     end_time = time.time()
     print("total time usage = %f sec" % (end_time - start_time))
     pass
